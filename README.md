c2bf3d00c0c3a1730758af9a7e685308841dec24
sushantbramhacharya <sushantbramacharya@gmail.com>
2023-12-06 11:46:49 +0545

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# nepali_ma_hiragana
+
+A hiragana Learning app designed for nepali peoples
+
+## Project Init
---
+++ android/app/src/main/kotlin/com/senbu/nepalimahiragana/nepali_ma_hiragana/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.senbu.nepalimahiragana.nepali_ma_hiragana
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
